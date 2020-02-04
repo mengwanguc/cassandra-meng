d5b564787e893bad0f494a00e4d38e28a83e1e5a
Meng-emulab <mengwanguc@gmail.com>
2020-02-04 16:15:31 -0700

    recompile.sh

--- recompile-cassandra.sh
+++ recompile-cassandra.sh
@@ -1,13 +1,5 @@
 export SOURCE_ROOT=/home/sda_mount
 
-sudo apt-get update
-sudo apt-get install -y git tar g++ make automake autoconf libtool  wget patch libx11-dev libxt-dev pkg-config texinfo locales-all unzip python
-sudo apt-get install -y openjdk-8-jre openjdk-8-jdk
-
-cd $SOURCE_ROOT/
-wget http://archive.apache.org/dist/ant/binaries/apache-ant-1.10.4-bin.tar.gz
-tar -xvf apache-ant-1.10.4-bin.tar.gz
-
 export LANG="en_US.UTF-8"
 export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8"
 export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64/"

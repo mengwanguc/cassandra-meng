766859e3f00a718aed38d434330cc39531af77ce
Meng Wang github desktop <mengwanguc@gmail.com>
2020-02-12 04:34:21 -0600

    debug

--- src/java/org/apache/cassandra/service/StorageProxy.java
+++ src/java/org/apache/cassandra/service/StorageProxy.java
@@ -1723,8 +1723,8 @@ public class StorageProxy implements StorageProxyMBean
     {
         int cmdCount = commands.size();
         
-//        System.out.println("@meng: fetching rows.....!!!");
-//        Thread.dumpStack();
+        System.out.println("@meng: fetching rows.....!!!");
+        Thread.dumpStack();
 
         SinglePartitionReadLifecycle[] reads = new SinglePartitionReadLifecycle[cmdCount];
         for (int i = 0; i < cmdCount; i++)

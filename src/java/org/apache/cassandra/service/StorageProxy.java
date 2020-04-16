f67b6dd2537476dcdde991840cb4629551216f22
Meng Wang github desktop <mengwanguc@gmail.com>
2020-04-16 10:37:01 -0500

    message set deadline

--- src/java/org/apache/cassandra/service/StorageProxy.java
+++ src/java/org/apache/cassandra/service/StorageProxy.java
@@ -2233,6 +2233,7 @@ public class StorageProxy implements StorageProxyMBean
                     System.out.println("    @meng: rangeCommand message to " + endpoint.getHostAddress());
                     MessageOut<ReadCommand> message = rangeCommand.createMessage(MessagingService.instance().getVersion(endpoint));
                     Tracing.trace("Enqueuing request to {}", endpoint);
+                    message.setDeadline(1);
                     MessagingService.instance().sendRRWithFailureMittcpu(message, endpoint, handler);
                 }
             }
